51de486c29aeca3b861d969adfa4fdf193eca880
Frank <asdj4030@gmail.com>
2017-12-04 21:43:51 +0800

    add head

--- src/main/java/com/bocom/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/bocom/service/impl/TransactionServiceImpl.java
@@ -33,31 +33,33 @@ public class TransactionServiceImpl implements TransactionService{
             Document doc = sb.build(source);
             Element root = doc.getRootElement();
             if(!root.getName().equals("ap")){//根元素不是ap的时候
-                return "错误信息1";
+                throw new JDOMException();
             }
             List childrens = root.getChildren();
-            if(childrens == null || childrens.size() != 1){
-                return "错误信息2";
+            if(childrens == null || childrens.size() != 2){
+                throw new JDOMException();
             }
             Element temp = null;
-            temp = (Element)childrens.get(0);
+            temp = (Element)childrens.get(1);
             if(!temp.getName().equals("body")){
-                return "错误信息3";
+                throw new JDOMException();
             }
             List grandChildrens = temp.getChildren();
             if(grandChildrens == null || grandChildrens.size() != 1){
-                return "错误信息4";
+                throw new JDOMException();
             }
             temp = (Element)grandChildrens.get(0);
             String targetAccount = temp.getValue();
             if(!temp.getName().equals("acno") || temp.getChildren().size() != 0 || targetAccount == null || targetAccount.trim().equals("")){
-                return "错误信息5";
+                throw new JDOMException();
             }
 
             List<TransactionDetail> results = tranactionMapper.selectByAccount(targetAccount);//得到交易明细数据的List
 
             Element resultRoot = new Element("ap");
             Document resultDoc = new Document(resultRoot);
+
+            resultRoot.addContent(getHead("success", "成功", "0000", ""));
 
             Element children = new Element("body");
             Element grandChildren1 = new Element("serial_record");
@@ -128,11 +130,72 @@ public class TransactionServiceImpl implements TransactionService{
             xmlOut.output(resultDoc, byteRsp);
             String finalString = byteRsp.toString("utf-8");
             return finalString.substring(39);
-        }catch (JDOMException e){
-            //dosomething
-        }catch (IOException e2){
-            //dosomething
+        }catch (Exception e){
+            Element resultRoot = new Element("ap");
+            Document resultDoc = new Document(resultRoot);
+            resultRoot.addContent(getHead("failed", "查询失败", "0001", "参数不正确"));
+            Format format = Format.getCompactFormat();
+            format.setEncoding("utf-8");
+            format.setIndent(" ");
+            XMLOutputter xmlOut = new XMLOutputter(format);
+            ByteArrayOutputStream byteRsp = new ByteArrayOutputStream();
+
+            try {
+                xmlOut.output(resultDoc, byteRsp);
+                String finalString = byteRsp.toString("utf-8");
+                return finalString.substring(39);
+            }catch (IOException e3){
+                e3.printStackTrace();
+            }
         }
-        return "错误信息6";
+        return "";
+    }
+
+    private Element getHead(String ans_code, String ans_info, String particular_code, String particular_info){
+        Element headerChildren = new Element("head");
+        Element headerGrandChildren1 = new Element("tr_code");
+        Element headerGrandChildren2 = new Element("corp_no");
+        Element headerGrandChildren3 = new Element("req_no");
+        Element headerGrandChildren4 = new Element("serial_no");
+        Element headerGrandChildren5 = new Element("ans_no");
+        Element headerGrandChildren6 = new Element("next_no");
+        Element headerGrandChildren7 = new Element("tr_acdt");
+        Element headerGrandChildren8 = new Element("tr_time");
+        Element headerGrandChildren9 = new Element("ans_code");
+        Element headerGrandChildren10 = new Element("ans_info");
+        Element headerGrandChildren11 = new Element("particular_code");
+        Element headerGrandChildren12 = new Element("particular_info");
+        Element headerGrandChildren13 = new Element("atom_tr_count");
+        Element headerGrandChildren14 = new Element("reserved");
+
+        headerChildren.addContent(headerGrandChildren1);
+        headerGrandChildren1.addContent("交易码");
+        headerChildren.addContent(headerGrandChildren2);
+        headerGrandChildren2.addContent("企业代码");
+        headerChildren.addContent(headerGrandChildren3);
+        headerGrandChildren3.addContent("发起方序号");
+        headerChildren.addContent(headerGrandChildren4);
+        headerGrandChildren4.addContent("交易序号");
+        headerChildren.addContent(headerGrandChildren5);
+        headerGrandChildren5.addContent("应答流水号");
+        headerChildren.addContent(headerGrandChildren6);
+        headerGrandChildren6.addContent("下笔交易序号");
+        headerChildren.addContent(headerGrandChildren7);
+        headerGrandChildren7.addContent("交易日期");
+        headerChildren.addContent(headerGrandChildren8);
+        headerGrandChildren8.addContent("时间");
+        headerChildren.addContent(headerGrandChildren9);
+        headerGrandChildren9.addContent(ans_code);
+        headerChildren.addContent(headerGrandChildren10);
+        headerGrandChildren10.addContent(ans_info);
+        headerChildren.addContent(headerGrandChildren11);
+        headerGrandChildren11.addContent(particular_code);
+        headerChildren.addContent(headerGrandChildren12);
+        headerGrandChildren12.addContent(particular_info);
+        headerChildren.addContent(headerGrandChildren13);
+        headerGrandChildren13.addContent("原子交易数");
+        headerChildren.addContent(headerGrandChildren14);
+        headerGrandChildren14.addContent("保留字段");
+        return headerChildren;
     }
 }
